cc91a04382f6606e8df598d96755ff779d9c82bd
root <root@milan-3.sh.intel.com>
2022-10-24 06:17:35 +0000

    fix model

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-pip uninstall timm detectron2
+pip uninstall timm detectron2 transformers
+pip install transformers
 python -m pip install 'git+https://github.com/facebookresearch/detectron2.git@v0.5'
 
 python inference.py  --device cuda --precision float16 --jit --nv_fuser --profile
